da8010891149c08d3d0bd1488bcc9c1966b4f05c
Jorge Ricardo <jorge.ricardo@prdavinci.tech>
2025-03-14 14:52:34 -0500

    Add Firebase configuration and integrate Angular Material components

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBxBHBEIAyqiwR5bSpE-75ZGOwOjIOG2uE",
+        authDomain: "movies-21b09.firebaseapp.com",
+        projectId: "movies-21b09",
+        storageBucket: "movies-21b09.firebasestorage.app",
+        messagingSenderId: "655057560492",
+        appId: "1:655057560492:web:526f51a9061c37587f5009",
+        measurementId: "G-TLCX1GXP2L"
+    },
+    tmdbApi: {
+      baseUrl: 'https://api.themoviedb.org/3',
+      apiKey: '11191464518afac982b4743eea805ca5'
+    }
+  };
